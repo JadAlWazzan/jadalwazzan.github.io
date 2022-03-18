77e78fb03ec46002c1aa4c346a5834debb694c1b
Jad AL Wazzan <67378553+JadAlWazzan@users.noreply.github.com>
2022-03-18 13:15:58 -0400

    Add files via upload

---
+++ data/projects/webprojects.js
@@ -0,0 +1,18 @@
+export default [
+	{
+		projectName: "Authenticating, Fetching & Manipulating Data from an API",
+		image: "images/Api-Challenge2.png",
+		summary:
+			"Developed a Web application to authenticate, GET & POST a list of transactions fetched from API. Filtere date as needed, and hosted it on a server.",
+		preview: "https://github.com/JadAlWazzan/APIchallenge",
+		techStack: ["JavaScript", "HTML", "PHP", "JQuery", "Ajax", "Heroku"],
+	},
+	{
+		projectName: "Web Portfolio",
+		image: "images/web-portfolio.png",
+		summary:
+			"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+		preview: "https://github.com/JadAlWazzan/jadalwazzan.github.io",
+		techStack: ["HTML", "Bootstrap", "JavaScript"],
+	}
+];
